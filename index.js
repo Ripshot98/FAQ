f87311f1b76536dd2ece3b666b9bac1e631db3d4
Ripshot98 <167313664+Ripshot98@users.noreply.github.com>
2025-01-12 20:10:44 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var que_head = document.querySelectorAll(".head_part");
+
+const handleView = (y) => {
+  for(let i=0; i<=4; i++) {
+    let elem = que_head[i];
+    btn_arrow = elem.querySelector(".btn_arrow"),
+    ans_text = elem.parentNode.querySelector(".text_part");
+
+if(elem == y){
+    elem.classList.toggle("active");
+    btn_arrow.classList.toggle("active");
+    ans_text.classList.toggle("active");
+    
+}else{
+    elem.classList.remove("active");
+    btn_arrow.classList.remove("active");
+    ans_text.classList.remove("active");
+}
+}
+}
